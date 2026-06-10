3f7ad234b974ef1870cb92c4f2b0ec097495f8d4
ravinahardkoretech-rs <ravina.hardkore.tech@gmail.com>
2026-06-10 15:24:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    function darkTheme() {
+   var element = document.body;
+    element.classList.toggle("dark-mode");
+}
---
+++ to-do.js
@@ -0,0 +1,45 @@
+let tasks = [];
+function displayTasks() {
+    let html = "";
+    for (let i = 0; i < tasks.length; i++) {
+        html +=
+            "<li>" +
+            tasks[i] +
+            "<button onclick='removeTask(" +
+            i +
+            ")';>X</button></li>";
+    }
+    document.getElementById("list").innerHTML = html;
+}
+function addTask() {
+    let taskInput = document.getElementById("task");
+    let text = taskInput.value;
+    if (text === "") {
+        return;
+    }
+    tasks.push(text);
+    taskInput.value = "";
+    saveTasks();
+    displayTasks();
+}
+function removeTask(i) {
+    tasks.splice(i, 1);
+    saveTasks();
+    displayTasks();
+}
+function clearAll() {
+    tasks = [];
+    saveTasks();
+    displayTasks();
+}
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function loadTasks() {
+    let saved = localStorage.getItem("tasks");
+    if (saved !== null) {
+        tasks = JSON.parse(saved);
+    }
+}
+loadTasks();
+displayTasks();
