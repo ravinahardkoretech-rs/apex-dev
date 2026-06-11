e477f0e4ce09b8a8cb929fad098073713c89132a
ravinahardkoretech-rs <ravina.hardkore.tech@gmail.com>
2026-06-11 14:24:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const navList = document.getElementById('nav-list');
 menuToggle.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
+const modal = document.getElementById("projectModal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close-btn");
+
+openBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
