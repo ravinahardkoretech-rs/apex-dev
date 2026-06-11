2371162ad159624d6770cefff5a9e55e175be2f4
ravinahardkoretech-rs <ravina.hardkore.tech@gmail.com>
2026-06-11 12:05:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
    var element = document.body;
     element.classList.toggle("dark-mode");
 }
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list');
+
+menuToggle.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
